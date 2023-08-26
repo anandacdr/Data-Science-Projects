62942a3a0626ff66da84a1b1f37a35afec30267c
anandacdr <jazzw815@gmail.com>
2023-08-26 17:47:36 +0545

    setupfile update

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT = '-e .'
+HYPEN_E_DOT='-e .'
 
 def get_requirements(file_path:str)->List[str]:
-    ''' This Function will return the list of Requirements ''' 
+    '''
+    this function will return the list of requirements
+    '''
     requirements=[]
 
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace('\n', "") for req in requirements]
+        requirements=[req.replace('\n',"") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
-
-
 setup(
     name="DSProjects",
     version="0.0.1",
     author="Ananda",
     author_email="info.anandachaudhary@gmail.com",
-    packages=find_packages,
+    packages=find_packages(),
     install_requires=get_requirements("requirements.txt"),
 )
 
